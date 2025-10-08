9bee144e45a9cb3cf3b47da8a8b142551d2c4961
DanielSaldanha <danielpedrosaldanha@gmail.com>
2025-10-08 16:42:20 -0300

    add: organização de controllers

--- HobbyAPI/Controllers/LogsController.cs
+++ HobbyAPI/Controllers/LogsController.cs
@@ -1,79 +1,24 @@
 ﻿using HobbyAPI.Data;
 using HobbyAPI.Model;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
-using System.ComponentModel;
 
 namespace HobbyAPI.Controllers
 {
-    [Route("api")]
-    [ApiController]
-    public class HobbyController : ControllerBase
+    public class LogsController : ControllerBase
     {
         private readonly AppDbContext _context;
-        public HobbyController(AppDbContext context)
+
+        public LogsController(AppDbContext context)
         {
             _context = context;
         }
-
-        [HttpPost("habits")]
-        public async Task<IActionResult> CreateHabit([FromBody] DTO habit)
-        {
-            if (habit == null)
-            {
-                return BadRequest("Preencha para criar um hábito");
-            }
-            var res = await _context.Habits.FirstOrDefaultAsync(x => x.name == habit.name);
-            if (res != null)
-            {
-                return BadRequest("esta tarefa ja foi registrada por você");
-            }
-
-
-            // Validação do goalType
-            GoalType goalType;
-
-            if (habit.goalType == "bool")
-            {
-                if (habit.goal != 1 || habit.goal != 0)
-                {
-                    return BadRequest("Esse valor é imcompativel com goal");
-                }
-                goalType = GoalType.Bool;
-            }
-            else if (habit.goalType == "count")
-            {
-                goalType = GoalType.Count;
-            }
-            else
-            {
-                return BadRequest("goalType deve ser 'bool' ou 'count'.");
-            }
-
-            Habit TrueHabit = new Habit
-            {
-                name = habit.name,
-                goal = habit.goal,
-                goalType = goalType, // Atribuindo o goalType corretamente
-                createdAt = DateOnly.FromDateTime(DateTime.Now),
-                updatedAt = DateOnly.FromDateTime(DateTime.Now),
-                clientId = habit.clientId
-            };
-
-            await _context.Habits.AddAsync(TrueHabit);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(CreateHabit), new { id = habit.Id }, habit);
-        }
-
         [HttpPost("logs")]
         public async Task<IActionResult> CreateLog(int id, string clientId)
         {
             var res = await _context.Habits.FirstOrDefaultAsync
                 (u => u.Id == id && u.clientId == clientId);
-            if(res == null)
+            if (res == null)
             {
                 return NotFound("você não possui essa tarefa");
             }
@@ -89,7 +34,7 @@ namespace HobbyAPI.Controllers
                 return BadRequest("Você não pode fazer dois logs deste mesmo Hábito por dia");
             }
 
-            if(verify != null)
+            if (verify != null)
             {
                 verify.date = DateOnly.FromDateTime(DateTime.Now);
                 verify.amount = verify.amount + 1;
@@ -108,7 +53,7 @@ namespace HobbyAPI.Controllers
                 await _context.Badges.AddAsync(badge);
                 //salve todas as mudanças
                 await _context.SaveChangesAsync();
-                return Ok("parabens por ter cumprido esta missão");
+                return NoContent();
             }
             //configurando material do log
             var log = new Logs
@@ -137,7 +82,7 @@ namespace HobbyAPI.Controllers
             await _context.HabitsLogs.AddAsync(log);
             //salvar
             await _context.SaveChangesAsync();
-            return Ok("parabens por ter cumprido esta missão");
+            return CreatedAtAction(nameof(CreateLog), new { id = log.Id }, log);
         }
 
         [HttpPost("ClaimBadges")]
@@ -170,10 +115,10 @@ namespace HobbyAPI.Controllers
                 badgeAL.date = DateOnly.FromDateTime(DateTime.Now);
                 badgeAL.badge = Badg3.Bronze;
                 await _context.SaveChangesAsync();
-                return Ok("parabens você ganhou uma medalha");
+                return NoContent();
             }
 
-            if(badgeAL.date < limite)
+            if (badgeAL.date < limite)
             {
                 badgeAL.consistency = 0;
                 await _context.SaveChangesAsync();
@@ -183,61 +128,8 @@ namespace HobbyAPI.Controllers
             badgeAL.consistency = badgeAL.consistency + 1;
             badgeAL.date = DateOnly.FromDateTime(DateTime.Now);
             await _context.SaveChangesAsync();
-            return Ok("efetuado com sucesso");
+            return CreatedAtAction(nameof(CreateBadge), new { id = badgeAL.Id }, badgeAL);
         }
-
-        [HttpGet("habits")]
-        public async Task<ActionResult> GetAll(string clientId)
-        {
-            var response = await _context.Habits
-           .Where(h => h.clientId == clientId)
-           .ToListAsync();
-
-            if (response == null || !response.Any())
-            {
-                return NotFound("dados não achados");
-            }
-
-            var TrueValue = response.Select(u => new NewDTO
-            {
-                Id = u.Id,
-                name = u.name,
-                goalType = u.goalType == GoalType.Bool ? "bool" : "count",
-                goal = u.goal == 0 && u.goalType == GoalType.Bool ? "false"
-                      : u.goal == 1 && u.goalType == GoalType.Bool ? "true"
-                      : u.goal.ToString(),
-            }).ToList();
-
-            return Ok(TrueValue);
-        }
-
-        [HttpGet("habits/{id}")]
-        public async Task<ActionResult> GetById(int id, string clientId)
-        {
-            var habit = await _context.Habits.FindAsync(id);
-            
-            if (habit == null)
-            {
-                return NotFound("falha ao visualizar hábitos");
-            }
-
-            if(habit.clientId != clientId)
-            {
-                return NotFound("Você não fez essa tarefa");
-            }
-
-            NewDTO response = new NewDTO
-            {
-                Id = habit.Id,
-                name = habit.name,
-                goalType = habit.goalType == GoalType.Bool ? "bool" : "count", // Convertendo enum para string
-                goal = habit.goal == 0 && habit.goalType == GoalType.Bool ? "false"
-                      : habit.goal == 1 && habit.goalType == GoalType.Bool ? "true"
-                      : habit.goal.ToString(),
-            };
-            return Ok(response);
-        }
-
         [HttpGet("stats/weekly")]
         public async Task<ActionResult> GetByWeekly(string clientId)
         {
@@ -247,7 +139,7 @@ namespace HobbyAPI.Controllers
             var habitos = await _context.HabitsLogs
                 .Where(h => h.date >= limite && h.clientId == clientId)
                 .ToListAsync();
-            if(!habitos.Any())
+            if (!habitos.Any())
             {
                 return NotFound("nenhum log realizado nesta semana");
             }
@@ -258,7 +150,7 @@ namespace HobbyAPI.Controllers
                 HabitId = u.HabitId,
                 name = u.name,
                 date = u.date,
-                goalType = u.goalType == GoalType.Bool ? "Bool" : "Count", 
+                goalType = u.goalType == GoalType.Bool ? "Bool" : "Count",
                 amount = u.amount.ToString()
             });
             return Ok(TrueValue);
@@ -268,10 +160,11 @@ namespace HobbyAPI.Controllers
         public async Task<ActionResult> GetByBadge(string badge, string clientId)
         {
 
-            if (badge == "bronze"){
+            if (badge == "bronze")
+            {
                 var res = await _context.Badges.Where(h => h.badge == Badg3.Bronze
                 && h.clientId == clientId).ToListAsync();
-                return Ok(res); 
+                return Ok(res);
             }
 
             if (badge == "prata")
@@ -280,10 +173,10 @@ namespace HobbyAPI.Controllers
                 && h.clientId == clientId).ToListAsync();
                 return Ok(res);
             }
-            
+
             if (badge == "ouro")
             {
-                var res = await _context.Badges.Where(h => h.badge == Badg3.Gold 
+                var res = await _context.Badges.Where(h => h.badge == Badg3.Gold
                 && h.clientId == clientId).ToListAsync();
                 return Ok(res);
             }
@@ -291,30 +184,7 @@ namespace HobbyAPI.Controllers
             return NotFound("você não possui esse tipo de medalhas");
         }
 
-        [HttpPut("habits/{id}")]
-        public async Task<IActionResult> PutHabit(int id,string clientId,string name, string type, int goal)
-        {
-
-            if (type == "bool" && goal > 1 || goal < 0)
-                return BadRequest("Você não pode botar valores imcompativeis em seu hábito");
-
-            var habit = await _context.Habits.FirstOrDefaultAsync(u => u.Id == id && u.clientId == clientId);
-            if(habit == null) return NotFound("usuario não encontrado");
-
-            habit.name = name;
-
-            if (type == "bool") habit.goalType = GoalType.Bool;
-            else if (type == "count") habit.goalType = GoalType.Count;
-
-            habit.goal = goal;
-
-            habit.updatedAt = DateOnly.FromDateTime(DateTime.Now);
-            
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [HttpPut("VerifyAmount")]
+        [HttpPut("verifyAmount")]
         public async Task<IActionResult> VerifyAmountsFromLogs(string clientId)
         {
             var hoje = DateOnly.FromDateTime(DateTime.Now);
@@ -340,24 +210,8 @@ namespace HobbyAPI.Controllers
 
             await _context.SaveChangesAsync();
 
-            return Ok("efetuado com seucesso");
-        }
-
-        [HttpDelete("habits/{id}")]
-        public async Task<IActionResult> DeleteHabit(int? id, string clientId)
-        {
-            if(id == null)
-            {
-                return BadRequest("Você precisa colocar o Id da tarefa");
-            }
-            var res = await _context.Habits.FirstOrDefaultAsync(u => u.Id == id && u.clientId == clientId);
-            if (res == null)
-            {
-                return BadRequest("falha ao encontrar habito");
-            }
-            _context.Habits.Remove(res);
-            await _context.SaveChangesAsync();
             return NoContent();
         }
+
     }
 }
